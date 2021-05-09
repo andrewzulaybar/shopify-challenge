3756ad14a7b7f12780ae15b7b7653b114b8e6dec
Andrew Zulaybar <andrewzulaybar@gmail.com>
2021-05-09 00:44:19 -0700

    fix(nominations): Add header and description text

--- src/components/NominatedList.tsx
+++ src/components/NominatedList.tsx
@@ -8,6 +8,17 @@ interface Props {
 }
 
 export default function NominatedList(props: Props) {
+  if (props.movies?.length === 0) {
+    return (
+      <div className="py-8">
+        <p className="text-sm text-gray-400">
+          Your nomination list is currently empty. Use the search functionality
+          to find movies to nominate.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="gap-4 grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 py-8">
       {props.movies?.map((movie) => (
--- src/pages/nominations.tsx
+++ src/pages/nominations.tsx
@@ -7,7 +7,7 @@ import { MovieDTO } from '../services/OMDBService';
 export default function NominationsPage() {
   const { state, dispatch } = useMoviesContext();
 
-  const [modifiedList, setModifiedList] = useState<Set<MovieDTO>>(new Set());
+  const [modified, setModified] = useState<Set<MovieDTO>>(new Set());
   const [movieList, setMovieList] = useState<MovieDTO[]>(
     Array.from(state.nominated)
   );
@@ -16,11 +16,11 @@ export default function NominationsPage() {
     (movie: MovieDTO) => {
       setMovieList([...movieList, movie]);
 
-      const modifiedMovies = new Set(modifiedList);
+      const modifiedMovies = new Set(modified);
       modifiedMovies.delete(movie);
-      setModifiedList(modifiedMovies);
+      setModified(modifiedMovies);
     },
-    [modifiedList, movieList]
+    [modified, movieList]
   );
 
   const removeMovie = useCallback(
@@ -29,10 +29,10 @@ export default function NominationsPage() {
       newMovies.splice(newMovies.indexOf(movie), 1);
       setMovieList(newMovies);
 
-      const modifiedMovies = new Set(modifiedList).add(movie);
-      setModifiedList(modifiedMovies);
+      const modifiedMovies = new Set(modified).add(movie);
+      setModified(modifiedMovies);
     },
-    [modifiedList, movieList]
+    [modified, movieList]
   );
 
   const updateNominationList = () => {
@@ -40,27 +40,50 @@ export default function NominationsPage() {
       type: DispatchAction.UPDATE_NOMINATIONS,
       payload: { movies: movieList },
     });
+
+    setModified(new Set());
   };
 
+  const maximumMoviesToNominate = 5;
+  const moviesLeftToNominate = maximumMoviesToNominate - movieList.length;
+
+  let message = `You have ${moviesLeftToNominate} more ${
+    moviesLeftToNominate === 1 ? 'movie' : 'movies'
+  } left to nominate!`;
+  if (moviesLeftToNominate === 0) {
+    message = "You're all done!";
+  } else if (moviesLeftToNominate < 0) {
+    message = `Please remove ${-moviesLeftToNominate} ${
+      -moviesLeftToNominate === 1 ? 'movie' : 'movies'
+    }!`;
+  }
+
   return (
-    <>
-      {modifiedList.size > 0 && (
-        <div className="flex flex-row justify-end px-8 w-full">
-          <button
-            className="bg-blue-400 border-2 border-blue-400 font-semibold p-2 px-4 rounded-3xl text-lg text-white 
-            transition ease-in-out duration-500 
-            focus:outline-none hover:bg-white hover:border-blue-400 hover:text-blue-400"
-            onClick={updateNominationList}
-          >
-            Update Nominations
-          </button>
+    <div className="px-12 pb-12">
+      <div className="flex flex-col">
+        <div className="flex flex-row justify-between items-center mb-2">
+          <h2 className="font-semibold mb-0 text-3xl">Nominated Movies</h2>
+          {modified.size > 0 && (
+            <button
+              className="bg-blue-400 border-2 border-blue-400 font-semibold py-1 px-4 rounded-3xl text-base text-white 
+                transition ease-in-out duration-500 
+                focus:outline-none hover:bg-white hover:border-blue-400 hover:text-blue-400"
+              onClick={updateNominationList}
+            >
+              Update Nominations
+            </button>
+          )}
         </div>
-      )}
+        <p className="text-sm">
+          Manage the list of movies you believe should be up for nomination. You
+          can have a <strong>maximum of 5 nominees</strong>. {message}
+        </p>
+      </div>
       <NominatedList
         movies={Array.from(state.nominated)}
         onNominate={addMovie}
         onRemoveNomination={removeMovie}
       />
-    </>
+    </div>
   );
 }
